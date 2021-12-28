f5e2f0627c66f0bd34f29ae2a98323f8ad95b471
Mathys <mathysdubois85@gmail.com>
2021-12-28 23:16:01 +0100

    continuité du vuejs

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
-new Vue({
+let vm = new Vue({
     el: '#bottom',
     data: {
         message: "Welcome everybody",
-        link: "https://vuejs.org/"
+        link: "https://vuejs.org/",
+        cls: 'success',
+        persons: ['Mathys', 'Cesar', 'Celian', 'Mattis'],
+        // style: { background: '#3A961C'}
+    },
+
+    methods: {
+        close: function () {
+            this.success = false
+            this.message = "fermé"
+            return { background: '#3A961C' }    
+        },
+
+        style: function () {
+            if (this.success) {
+                return {background: '#00FF00'}
+            }else {
+                return {background: '#FF0000'}
+            }
+        },
+
+        addPerson: function () {
+            this.persons.push('Marion')
+        }
+
+        // st: function () {
+        //     if (this.success == true) {
+        //         return { backgroud: '#00FF00' }
+        //     } else {
+        //         return { background: '#FF0000' }
+    
+        // }
     }
 })
